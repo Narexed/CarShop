1000d5238755f2af7480e275c0c8caf19f271148
AvetikNersisyan <nersisyan.avetik@gmail.com>
2022-09-17 20:56:55 +0400

    testing commit

--- narek.js
+++ narek.js
@@ -1,19 +1,14 @@
-const buttonEl = document.querySelectorAll(".button")
-const cartEl = document.querySelector(".cart")
+const buttonEl = document.querySelectorAll(".button");
+const cartEl = document.querySelector(".cart");
 
+// comment to test git
 
 let cart = 0;
 
-
-let a = buttonEl.forEach(buttonEl =>{
-
-   buttonEl.addEventListener("click", (ev)=>{
-
-cart = cart + +ev.target.title
-cartEl.innerText = cart + "$"
-console.log(cartEl.innerText)
-    })
-})
-
-
-
+let a = buttonEl.forEach((buttonEl) => {
+  buttonEl.addEventListener("click", (ev) => {
+    cart = cart + +ev.target.title;
+    cartEl.innerText = cart + "$";
+    console.log(cartEl.innerText);
+  });
+});
